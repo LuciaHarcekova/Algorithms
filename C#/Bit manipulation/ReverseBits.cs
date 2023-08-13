4a0dd1285244ac1a004ff1ca94bbafa713f8cd6f
Lucia Harcekova <lharcekova@microsoft.com>
2023-08-13 18:30:43 +0100

    - "Reverse Bits " solution in Java, Python, C# and C++

---
+++ C#/Bit manipulation/ReverseBits.cs
@@ -0,0 +1,45 @@
+/*
+190. Reverse Bits
+Link: https://leetcode.com/problems/reverse-bits/
+
+Reverse bits of a given 32 bits unsigned integer.
+
+Note:
+Note that in some languages, such as Java, there is no unsigned integer type.
+In this case, both input and output will be given as a signed integer type.
+They should not affect your implementation, as the integer's internal binary
+representation is the same, whether it is signed or unsigned.
+In Java, the compiler represents the signed integers using 2's complement notation.
+Therefore, in Example 2 above, the input represents the signed integer -3 and the
+output represents the signed integer -1073741825.
+ 
+Example 1:
+Input: n = 00000010100101000001111010011100
+Output:    964176192 (00111001011110000010100101000000)
+Explanation: The input binary string 00000010100101000001111010011100 represents the 
+unsigned integer 43261596, so return 964176192 which its binary representation is
+00111001011110000010100101000000.
+
+Example 2:
+Input: n = 11111111111111111111111111111101
+Output:   3221225471 (10111111111111111111111111111111)
+Explanation: The input binary string 11111111111111111111111111111101 represents the
+unsigned integer 4294967293, so return 3221225471 which its binary representation is
+10111111111111111111111111111111.
+*/
+
+public class Solution {
+    public uint reverseBits(uint n) {
+        uint result = 0;
+
+        for (int i =0; i < 32; i++){
+            // get the last bit from input n on the position i from end (we need to shift by i bits)
+            uint bit = (n >> i) & 1;
+
+            // add the bit to the result (or operation) on the correct possition (shifting by 31-i places)
+            result |= bit << (31 - i);
+        }
+
+        return result;
+    }
+}
